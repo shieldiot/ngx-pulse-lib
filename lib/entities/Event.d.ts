9bdb6496c83e95ad9e47a0ef4f27f7a416cac135
BitBucket Pipeline <bitbucket@shieldiot.io>
2023-12-21 16:58:10 +0000

    Update lib:1.16.143

--- lib/entities/Event.d.ts
+++ lib/entities/Event.d.ts
@@ -1,13 +1,13 @@
-import { Indicator } from '../common/Indicator';
-import { RuleTypeCode } from '../enums/RuleTypeCode';
-import { GeoData } from '../common/GeoData';
-import { BaseEntity } from '../entities/BaseEntity';
 import { EventTypeCode } from '../enums/EventTypeCode';
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
+import { GeoData } from '../common/GeoData';
+import { StringKeyValue } from '../common/StringKeyValue';
 import { EventCategoryCode } from '../enums/EventCategoryCode';
 import { EventOccurrence } from '../entities/EventOccurrence';
 import { EventStatusCode } from '../enums/EventStatusCode';
-import { StringKeyValue } from '../common/StringKeyValue';
+import { Indicator } from '../common/Indicator';
+import { RuleTypeCode } from '../enums/RuleTypeCode';
+import { BaseEntity } from '../entities/BaseEntity';
 export declare class Event extends BaseEntity {
     accountId: string;
     streamId: string;
--- lib/entities/EventWithDevice.d.ts
+++ lib/entities/EventWithDevice.d.ts
@@ -1,14 +1,14 @@
 import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { EventStatusCode } from '../enums/EventStatusCode';
-import { StringKeyValue } from '../common/StringKeyValue';
-import { BaseEntity } from '../entities/BaseEntity';
-import { EventTypeCode } from '../enums/EventTypeCode';
-import { RuleTypeCode } from '../enums/RuleTypeCode';
 import { GeoData } from '../common/GeoData';
 import { EventCategoryCode } from '../enums/EventCategoryCode';
 import { EventOccurrence } from '../entities/EventOccurrence';
 import { Device } from '../entities/Device';
+import { BaseEntity } from '../entities/BaseEntity';
+import { EventTypeCode } from '../enums/EventTypeCode';
 import { Indicator } from '../common/Indicator';
+import { RuleTypeCode } from '../enums/RuleTypeCode';
+import { StringKeyValue } from '../common/StringKeyValue';
 export declare class EventWithDevice extends BaseEntity {
     accountId: string;
     streamId: string;
