613b5e232bf857e17a4851d3bd0709492d218be6
BitBucket Pipeline <bitbucket@shieldiot.io>
2024-01-16 09:52:02 +0000

    Update lib:1.15.174

--- lib/common/TokenData.d.ts
+++ lib/common/TokenData.d.ts
@@ -1,6 +1,6 @@
-import { UserStatusCode } from '../enums/UserStatusCode';
 import { UserTypeCode } from '../enums/UserTypeCode';
 import { MemberRoleCode } from '../enums/MemberRoleCode';
+import { UserStatusCode } from '../enums/UserStatusCode';
 export declare class TokenData {
     accountId: string;
     subjectId: string;
