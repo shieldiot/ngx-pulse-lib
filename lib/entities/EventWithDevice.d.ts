98b075907ad9163dcc16842eab56df47296f9bfc
BitBucket Pipeline <bitbucket@shieldiot.io>
2024-01-16 09:53:34 +0000

    Update lib:1.17.173

--- lib/entities/EventWithDevice.d.ts
+++ lib/entities/EventWithDevice.d.ts
@@ -1,15 +1,15 @@
+import { GeoData } from '../common/GeoData';
+import { EventOccurrence } from '../entities/EventOccurrence';
 import { BaseEntity } from '../entities/BaseEntity';
+import { StringKeyValue } from '../common/StringKeyValue';
+import { EventCategoryCode } from '../enums/EventCategoryCode';
+import { TrafficDirectionCode } from '../enums/TrafficDirectionCode';
 import { EventTypeCode } from '../enums/EventTypeCode';
+import { SeverityTypeCode } from '../enums/SeverityTypeCode';
 import { EventStatusCode } from '../enums/EventStatusCode';
 import { Indicator } from '../common/Indicator';
 import { RuleTypeCode } from '../enums/RuleTypeCode';
-import { TrafficDirectionCode } from '../enums/TrafficDirectionCode';
 import { Device } from '../entities/Device';
-import { SeverityTypeCode } from '../enums/SeverityTypeCode';
-import { GeoData } from '../common/GeoData';
-import { StringKeyValue } from '../common/StringKeyValue';
-import { EventCategoryCode } from '../enums/EventCategoryCode';
-import { EventOccurrence } from '../entities/EventOccurrence';
 export declare class EventWithDevice extends BaseEntity {
     accountId: string;
     streamId: string;
