91be023030410357948f2f458f6373796b696efa
BitBucket Pipeline <bitbucket@shieldiot.io>
2024-06-09 08:57:47 +0000

    Update lib:1.15.267

--- lib/entities/AccountSettings.d.ts
+++ lib/entities/AccountSettings.d.ts
@@ -1,6 +1,6 @@
-import { HomePageViewCode } from '../enums/HomePageViewCode';
 import { ComplianceReportSettings } from '../entities/ComplianceReportSettings';
 import { TeaserPopUpSettings } from '../entities/TeaserPopUpSettings';
+import { HomePageViewCode } from '../enums/HomePageViewCode';
 export declare class AccountSettings {
     logo: string;
     defaultHomePageView: HomePageViewCode;
--- lib/entities/InsightSpec.d.ts
+++ lib/entities/InsightSpec.d.ts
@@ -1,7 +1,9 @@
 import { BaseEntity } from '../entities/BaseEntity';
+import { InsightStatusCode } from '../enums/InsightStatusCode';
 export declare class InsightSpec extends BaseEntity {
     accountId: string;
     streamId: string;
     queryId: string;
     enabled: boolean;
+    status: InsightStatusCode;
 }
